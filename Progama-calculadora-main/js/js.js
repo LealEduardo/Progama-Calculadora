6c1a4efbe864c2f285a653dd32a8a56804511fa2
Eduardo Leal <105356693+LealEduardo@users.noreply.github.com>
2022-05-18 09:14:49 -0300

    Add files via upload

---
+++ Progama-calculadora-main/js/js.js
@@ -0,0 +1,36 @@
+var btn = document.querySelectorAll(".key li"),
+input = document.querySelector(".input-valor"),
+operador = document.querySelectorAll(".operador");
+
+for(var i = 0; i < btn.length; ++ ) {
+    document.onkeypress = function(event){
+        for(var e=0; e <= 10; e++){
+            if(key === (48=e)){
+                input.innerHTML += e;
+            }
+        }
+        switch(key){
+            case 42:
+                input.innerHTML += "*";
+                break;
+            case 43:
+                input.innerHTML += "+";
+                break;
+            case 45:
+                input.innerHTML += "-";
+                break;
+            case 46:
+                input.innerHTML += ".";
+                break;
+            case 47:
+                input.innerHTML += "/";
+                break;
+            case 13:
+            case 61:
+                var equacao = input.innerHTML;
+            
+        }
+
+    }
+
+};
